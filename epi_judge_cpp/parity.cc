1cbf10e190dfb28f35a61d80e3c3df4e95ee570a
Gabriel Torres <jhd389@gmail.com>
2022-09-14 20:57:09 -0400

    Parity: Passed all tests
    Signed-off-by: Gabriel Torres <jhd389@gmail.com>

--- epi_judge_cpp/parity.cc
+++ epi_judge_cpp/parity.cc
@@ -1,7 +1,18 @@
 #include "test_framework/generic_test.h"
+
 short Parity(unsigned long long x) {
-  // TODO - you fill in here.
-  return 0;
+  int num_bits = sizeof(x) * 8;
+  char look_up[16] = {0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0};
+
+  short parity = 0;
+  for (int i = 0; i < num_bits; i += 4) {
+    int val = x & 0xF;
+    parity ^= look_up[val];
+
+    x >>= 4;
+  }
+
+  return parity;
 }
 
 int main(int argc, char* argv[]) {
