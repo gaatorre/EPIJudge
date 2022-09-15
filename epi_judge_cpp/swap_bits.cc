5d80114e64a46f3fb8272693f309c69e4535489a
Gabriel Torres <jhd389@gmail.com>
2022-09-14 21:24:13 -0400

    Swap bits: Passed all tests!
    I was off by not making my constants of type
    long long by adding "LL" at the end of the
    number.
    
    Signed-off-by: Gabriel Torres <jhd389@gmail.com>

--- epi_judge_cpp/swap_bits.cc
+++ epi_judge_cpp/swap_bits.cc
@@ -1,7 +1,14 @@
 #include "test_framework/generic_test.h"
 long long SwapBits(long long x, int i, int j) {
-  // TODO - you fill in here.
-  return 0;
+    /**
+     * Since the type is long long, we need "L"
+     * in order to extend past 32 bits.
+     */
+    if (((x >> i) & 0x1) != ((x >> j) & 0x1)) {
+        x ^= (1LL << i) | (1LL << j);
+    }
+
+    return x;
 }
 
 int main(int argc, char* argv[]) {
